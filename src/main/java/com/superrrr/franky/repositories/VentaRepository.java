2126f48506325abe4d868a6097a330f0bf5827c8
Diego Ortiz <zitro13@gmail.com>
2026-05-26 16:16:56 -0500

    Se crean los repositories

---
+++ src/main/java/com/superrrr/franky/repositories/VentaRepository.java
@@ -0,0 +1,9 @@
+package com.superrrr.franky.repositories;
+
+import com.superrrr.franky.model.Venta;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface VentaRepository extends JpaRepository<Venta, Long> {
+}
