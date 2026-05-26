98fe1f4f67a04c9fd5b63f13a0f0c9d00e5cfe6e
Diego Ortiz <zitro13@gmail.com>
2026-05-26 14:17:09 -0500

    Initial commit

---
+++ src/main/java/com/superrrr/franky/FrankyApplication.java
@@ -0,0 +1,13 @@
+package com.superrrr.franky;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FrankyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FrankyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/superrrr/franky/FrankyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.superrrr.franky;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FrankyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
